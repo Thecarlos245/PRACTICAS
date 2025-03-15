a837a2e9f96dcb4e58e03dec9b717cdc040f8431
Thecarlos245 <manzanitadores@gmail.com>
2025-03-14 21:42:43 -0400

    bit

--- Intropoo/Ejercicio1/Persona.py
+++ Intropoo/Ejercicio1/Persona.py
@@ -4,29 +4,29 @@ class Persona:
         self.edad = edad
         self.ciudad = ciudad
 
-    def __str__(self):
+    def mostrar(self):
         return f"Hola, soy {self.nombre} de {self.ciudad}"
 
-    def es_mayor_de_edad(self):
+    def mayorEdad(self):
         if(self.edad >= 18):
             print(f"{self.nombre} es mayor de edad")
         else:
             print(f"{self.nombre} es menor de edad")
 # a) Mostrar saludo
 p1 = Persona("Juan", 20, "Lima")
-print(p1)
+print(p1.mostrar)
 # b) Crea 3 persona y su saludo 
 p2 = Persona("Ana", 15, "Arequipa")
 p3 = Persona("Pedro", 30, "Cusco")
 p4 = Persona("Maria", 18, "Trujillo")
-print(p2)
-print(p3)
-print(p4)
+print(p2.mostrar)
+print(p3.mostrar)
+print(p4.mostrar)
 
 # c) Verificar si es mayor de edad
-p1.es_mayor_de_edad()
-p2.es_mayor_de_edad()
-p3.es_mayor_de_edad()
-p4.es_mayor_de_edad()
+p1.mayorEdad()
+p2.mayorEdad()
+p3.mayorEdad()
+p4.mayorEdad()
 
 
