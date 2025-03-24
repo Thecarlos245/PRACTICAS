2a5bc73afcaa63e149a46a36ed744f9fddd0db8c
Thecarlos245 <manzanitadores@gmail.com>
2025-03-24 16:57:24 -0400

    mito

--- Intropoo/Ejercicio1/Persona.java
+++ Intropoo/Ejercicio1/Persona.java
@@ -1,9 +1,9 @@
 package Intropoo.Ejercicio1;
 
 public class Persona {
-    int edad;
-    String nombre;
-    String ciudad;
+    private int edad;
+    private String nombre;
+    private String ciudad;
 
     public Persona(int edad, String nombre, String ciudad) {
         this.edad = edad;
@@ -11,7 +11,7 @@ public class Persona {
         this.ciudad = ciudad;
     }
 
-    public void mostrarSaludo() {
+    public void Mostrar() {
        System.out.println("Hola, soy " + nombre + " de " + ciudad);
     }
     
@@ -27,18 +27,19 @@ public class Persona {
 
 
 public static void main(String[] args) {
-    // TODO code application logic here
+    
             // a) Mostrar el saludo 
     Persona p1 = new Persona(21,"carlos","La Paz");
-    p1.mostrarSaludo();
+    p1.Mostrar();
     
     // b) Crea 3 personas y crea su saludo 
     Persona p2 = new Persona(10,"Mauricio","Beni");
     Persona p3 = new Persona(18,"Manuel","Santa Cruz");
     Persona p4 = new Persona(10,"Erland","La Paz");
-    p2.mostrarSaludo();
-    p3.mostrarSaludo();
-    p4.mostrarSaludo();
+    p2.Mostrar();
+    p3.Mostrar();
+    p4.Mostrar();
+    System.out.println(" ----- EDADES DE LAS PERSONAS -----");
     // c) verificar su edad 
     p1.VerificarEdad();
     p2.VerificarEdad();
--- Intropoo/Ejercicio1/Persona.py
+++ Intropoo/Ejercicio1/Persona.py
@@ -1,32 +1,32 @@
 class Persona:
-    def __init__(self, nombre, edad, ciudad):
-        self.nombre = nombre
+    def __init__(self, edad, nombre, ciudad):
         self.edad = edad
+        self.nombre = nombre
         self.ciudad = ciudad
 
-    def mostrar(self):
+    def Mostrar(self):
         return f"Hola, soy {self.nombre} de {self.ciudad}"
 
-    def mayorEdad(self):
+    def VerificarEdad(self):
         if(self.edad >= 18):
             print(f"{self.nombre} es mayor de edad")
         else:
             print(f"{self.nombre} es menor de edad")
 # a) Mostrar saludo
-p1 = Persona("Juan", 20, "Lima")
-print(p1.mostrar)
+p1 = Persona(21,"carlos","La Paz")
+print(p1.Mostrar())
 # b) Crea 3 persona y su saludo 
-p2 = Persona("Ana", 15, "Arequipa")
-p3 = Persona("Pedro", 30, "Cusco")
-p4 = Persona("Maria", 18, "Trujillo")
-print(p2.mostrar)
-print(p3.mostrar)
-print(p4.mostrar)
+p2 = Persona(10,"Mauricio","Beni")
+p3 = Persona(18,"Manuel","Santa Cruz")
+p4 = Persona(10,"Erland","La Paz")
+print(p2.Mostrar())
+print(p3.Mostrar())
+print(p4.Mostrar())
 
 # c) Verificar si es mayor de edad
-p1.mayorEdad()
-p2.mayorEdad()
-p3.mayorEdad()
-p4.mayorEdad()
+p1.VerificarEdad()
+p2.VerificarEdad()
+p3.VerificarEdad()
+p4.VerificarEdad()
 
 
